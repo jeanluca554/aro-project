b6600aa051fde60ef2e1248707fe225d796ad846
jeanluca554 <jeanluca554@hotmail.com>
2023-02-08 12:16:38 -0300

    feat: add scroll reveal on home page

--- src/components/areaCard.tsx
+++ src/components/areaCard.tsx
@@ -9,7 +9,7 @@ type AreaCardProps = {
 
 const AreaCard = (props: AreaCardProps) => {
     return (
-        <div className='bg-white rounded-t-3xl shadow-3xl border-b-4 border-orange-400 md:w-1/2'>
+        <div className='bg-white rounded-t-3xl shadow-3xl border-b-4 border-orange-400 md:h-64 lg:h-60'>
             <div className='p-4 flex items-center'>
                 <div>
                     <div className='w-14 h-14 rounded-full bg-orange-600 flex flex-col justify-center items-center'>
