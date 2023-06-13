4fd361f84291bea0eb891fcf5e816be1845e9542
Jean Luca <jean@jean-PC>
2023-06-13 12:52:26 -0300

    feat(pix transaction): show qr code

--- src/components/Forms/ReviewForm.tsx
+++ src/components/Forms/ReviewForm.tsx
@@ -26,10 +26,18 @@ export function ReviewForm({ data }) {
 
             </>
             :
-            <div className='flex flex-col'>
-              <h2 className='font-medium text-gray-600 pt-2'>Link do pix para pagamento:</h2>
-              <a className='mt-8 underline cursor-pointer font-bold text-gray-600' href={data.pixQrCode} target='_blank'>{data.pixQrCode}</a>
-            </div>
+            data.statusTransaction === 1
+              ?
+              <div className='flex flex-col'>
+                <h2 className='font-medium text-gray-600 pt-2'>Link do pix para pagamento:</h2>
+                <a className='mt-8 underline cursor-pointer font-bold text-gray-600' href={data.pixQrCode} target='_blank'>{data.pixQrCode}</a>
+                <img src={data.pixQrCode} alt="pix QR Code" />
+              </div>
+              :
+              <div className='flex flex-col'>
+                <h2 className='font-bold text-gray-600 pt-2'>Erro ao gerar o QR Code do pix</h2>
+                <p className='font-medium text-gray-600 pt-4'>Parece que estamos com problemas para gerar o QR Code. Tente novamente mais tarde.</p>
+              </div>
       }
     </div >
   )
